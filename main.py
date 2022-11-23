09ed82dfac6e0adb426f2313bbbfceb24cdfb1f2
GyuRi <owen.kim0708@gmail.com>
2022-11-23 20:11:44 +0900

    upload help file

---
+++ main.py
@@ -0,0 +1,12 @@
+class ClassName(object):
+	"""docstring for ClassName"""
+	def __init__(self, arg):
+		super(ClassName, self).__init__()
+		self.arg = arg
+
+	def function(self):
+        """독시즌 테스트 입니다."""
+	        pass
+
+def function():
+	pass
